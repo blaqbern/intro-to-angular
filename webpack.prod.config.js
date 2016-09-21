63f0226e5c2588a394f2543551585960ceba878b
blaqbern <blaqbern@users.noreply.github.com>
2016-09-21 08:14:42 -0400

    Updates

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
-    publicPath: '/build/',
+    publicPath: 'build/',
   },
   module: {
     noParse: /node_modules\/bespoke-theme-[\w-]+\/dist/,
@@ -16,7 +16,12 @@ module.exports = {
       { test: /\.md$/, loaders: ['html', 'highlight', 'markdown'] },
       { test: /\.html$/, loaders: ['html'] },
       { test: /\.js$/, loaders: ['babel'], exclude: /node_modules/ },
-      { test: /\.(png|jpg|woff2?|[ot]tf|eot|svg)$/, loaders: ['file'] }
+      { test: /\.(png|jpe?g|gif)$/, loaders: ['url?limit=10000'], exclude: /node_modules/ },
+      { test: /\.woff(?:\?[\d\w-\.=]+)?$/, loaders: ['url?limit=10000&mimetype=application/font-woff'], exclude: /node_modules/ },
+      { test: /\.woff2(?:\?[\d\w-\.=]+)?$/, loaders: ['url?limit=10000&mimetype=application/font-woff2'], exclude: /node_modules/ },
+      { test: /\.eot(?:\?[\d\w-\.=]+)?$/, loaders: ['url?limit=10000&mimetype=application/vnd.ms-fontobject'], exclude: /node_modules/ },
+      { test: /\.[ot]tf(?:\?[\d\w-\.=]+)?$/, loaders: ['url?limit=10000&mimetype=application/octet-stream'], exclude: /node_modules/ },
+      { test: /\.svg(?:\?[\d\w-\.=]+)?$/, loaders: ['url?limit=10000&mimetype=img/svg+xml'], exclude: /node_modules/ },
     ],
   },
 }
